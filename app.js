f9d67c70a56b7b81262df8f670a2057601b70d5d
Shih-Chun Yin <scyin0121@gmail.com>
2016-04-05 18:10:01 -0400

    add 3 websites to allow access

--- app.js
+++ app.js
@@ -65,4 +65,24 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://scying.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://codepen.io/scying');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 module.exports = app;
