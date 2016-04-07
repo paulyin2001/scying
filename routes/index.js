a07803bee7dde96bd5367903f2f236107cfd5f95
Shih-Chun Yin <scyin0121@gmail.com>
2016-04-07 17:18:27 -0400

    Finish weather app

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,10 @@ router.get('/', function(req, res, next) {
 router.get('/randomquote', function(req, res, next) {
   res.render('randomquote', { title: 'Random Quotes' });
 });
-
+/* GET random quote app. */
+router.get('/weather', function(req, res, next) {
+  res.render('weather', { title: 'Weather' });
+});
 
 
 module.exports = router;
